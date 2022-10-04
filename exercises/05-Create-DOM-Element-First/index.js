e975592583b201ba4f1cf8ce035c9b3f6e778cdf
gleonag <gleonag@gmail.com>
2022-10-04 18:02:23 +0000

    exercices

--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -1,1 +1,6 @@
 // Your code here
+const myElem = document.createElement("P")
+myElem.style.background = "yellow";
+myElem.innerHTML = "Hello World";
+document.body.appendChild(myElem);
+
--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-	
+	const container = document.createElement("DIV");
+	container.style.background = "yellow";
+	container.innerHTML = "Hello World";
+	document.body.appendChild(container);
 });
